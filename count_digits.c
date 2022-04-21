55485e3592e987779d65741e51bd5e5daf7388bb
prudhvi-karri <97424288+prudhvi-karri@users.noreply.github.com>
2022-04-21 18:51:52 +0530

    #include<stdio.h> int main() {     int n,i,k,temp,c1=0,rem,c2=0;     scanf("%d",&n);     int arr[n],a[n];     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);        // printf("%d ",arr[i]);     }    // printf(" ");     for(i=0;i<n;i++)     {         temp=arr[i];         while(temp!=0)         {             rem=temp%10;             c1++;             temp=temp/10;         }         if(arr[i]==0)             {                 c1++;             }         a[i]=c1;         printf("%d ",a[i]);         c1=0;     }
    }

---
+++ count_digits.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,k,temp,c1=0,rem,c2=0;
+    scanf("%d",&n);
+    int arr[n],a[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+       // printf("%d ",arr[i]);
+    }
+   // printf("
+");
+    for(i=0;i<n;i++)
+    {
+        temp=arr[i];
+        while(temp!=0)
+        {
+            rem=temp%10;
+            c1++;
+            temp=temp/10;
+        }
+        if(arr[i]==0)
+            {
+                c1++;
+            }
+        a[i]=c1;
+        printf("%d ",a[i]);
+        c1=0;
+    }
+    
+}
